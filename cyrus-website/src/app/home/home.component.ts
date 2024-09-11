1bddd72eb5a99c5bb5345c8a2d132305d132a597
Your Name <you@example.com>
2024-09-11 11:53:22 +0530

    services-page

--- cyrus-website/src/app/home/home.component.ts
+++ cyrus-website/src/app/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  private currentSlide: number = 0;
+  private slides: HTMLElement[] = [];
+  private totalSlides: number = 0;
 
+  ngAfterViewInit() {
+    this.slides = Array.from(document.querySelectorAll('.carousel-slide')) as HTMLElement[];
+    this.totalSlides = this.slides.length;
+    this.showSlide(this.currentSlide);
+    setInterval(() => this.nextSlide(), 3000); // Change slide every 3 seconds
+  }
+
+  private showSlide(index: number) {
+    this.slides.forEach((slide, i) => {
+      slide.style.opacity = (i === index) ? '1' : '0';
+    });
+  }
+
+  private nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.totalSlides;
+    this.showSlide(this.currentSlide);
+  }
 }
